9ec623b8ed733e0e15d08ce18ba723f16ebb3ad5
Jason Manuel <jama.indo@hotmail.com>
2021-01-10 21:01:47 -0700

    Add "Replace slide text" functionality, fix presentation insertion code

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 function App() {
   const [ presentation, setPresentation ] = useState(null);
@@ -33,7 +33,7 @@ function Presentation({ presentation }) {
   const [ text, setText ] = useState('');
 
   const downloadAnchorRef = useRef();
-  const downloadAnchor = <a href={`data:text/html,${encodeURIComponent(html)}`} download='presentation.html' style={{display: 'none'}} ref={downloadAnchorRef} aria-role='hidden'>Download HTML Presentation</a>;
+  const downloadAnchor = <a href={`data:text/html,${encodeURIComponent(html)}`} download='presentation.html' style={{display: 'none'}} ref={downloadAnchorRef} aria-hidden>Download HTML Presentation</a>;
 
   async function exportPresentation() {
     // NOTE: We already have the HTML, and I don't want to configure a proxy to
@@ -46,8 +46,8 @@ function Presentation({ presentation }) {
     appendToSlideArray(json);
   }
 
-  function attachToRevealJS(presentationElement) {
-    const Reveal = presentationElement.contentWindow.Reveal;
+  function attachToRevealJS() {
+    const Reveal = window.Reveal;
     Reveal.on('slidechanged', event => {
       setCurrentSlideIndex(event.indexh);
     });
@@ -57,7 +57,8 @@ function Presentation({ presentation }) {
     setText(event.target.value);
   }
 
-  async function onSubmitText() {
+  async function onSubmitText(event) {
+    event.preventDefault();
     const currentSlide = slides[currentSlideIndex];
     const { justBefore, justAfter } = currentSlide.slideCursor;
     const json = await (await fetch(`/text/replace?justBefore=${justBefore}&justAfter=${justAfter}&presentationID=${presentation.presentationID}&text=${text}`));
@@ -65,9 +66,12 @@ function Presentation({ presentation }) {
     setSlides([...slides]);
   }
 
-  function afterInsertPresentation() {
-    window.Reveal.initialize({ embedded: true });
-  }
+  const tryInitializeReveal = useCallback(() => {
+    if (window.Reveal && !window.Reveal.isReady()) {
+      window.Reveal.initialize({ embedded: true });
+      attachToRevealJS();
+    }
+  }, []);
 
   useEffect(() => {
     setLoading(true);
@@ -81,56 +85,94 @@ function Presentation({ presentation }) {
   return (
     <>
       <form>
-        <button onClick={exportPresentation}>Export presentation as HTML</button>
+        <button onClick={exportPresentation} type='button'>Export presentation as HTML</button>
         {downloadAnchor}
-        <button onClick={addSlide}>Add new slide</button>
+        <button onClick={addSlide} type='button'>Add new slide</button>
         <label>
           Replace slide content with text:
-          <input onChange={onTextChange} value={text} />
+          <input onChange={onTextChange} value={text} type='text' />
         </label>
         <button onClick={onSubmitText} type='submit'>Submit text</button>
       </form>
       {/* Use something other than iframe since we need to interact with
       content. slides.com doesn't use iframe */}
-      {loading ? <p>Loading presentation...</p> : <ArbitraryHTMLDocument html={html} afterInsertHTML={afterInsertPresentation}></ArbitraryHTMLDocument>}
+      {loading ? <p>Loading presentation...</p> : <ArbitraryHTMLDocument html={html} style={{height: '16em'}} afterInsertScript={tryInitializeReveal} shouldInsertScript={shouldInsertScript}></ArbitraryHTMLDocument>}
     </>
   );
 }
 
-function ArbitraryHTMLDocument({ html, afterInsertHTML }) {
-  // const [bodyHTML, setBodyHTML] = useState('');
+function shouldInsertScript(element)  {
+  return !element.text;
+};
+
+const arbitraryHTMLDocumentDefaultProps = {
+  afterInsertHTML() { return null; },
+  style: {},
+  afterInsertScript() { return null; },
+  shouldInsertScript() { return true; }
+};
+
+function ArbitraryHTMLDocument({
+  html,
+  afterInsertHTML = arbitraryHTMLDocumentDefaultProps.afterInsertHTML,
+  style = arbitraryHTMLDocumentDefaultProps.style,
+  afterInsertScript = arbitraryHTMLDocumentDefaultProps.afterInsertScript,
+  shouldInsertScript = arbitraryHTMLDocumentDefaultProps.shouldInsertScript
+}) {
   const contentRef = useRef();
   const templateRef = useRef();
 
-  function manipulateHTML(templateElement) {
-    if (!templateElement) { return; }
+  useEffect(() => {
+    function manipulateHTML(templateElement) {
+      const scriptPromises = [];
 
-    const documentFragment = templateElement.content;
-    if (contentRef.current) {
-      for (let child of documentFragment.children) {
-        if (child.tagName === 'SCRIPT') {
-          // https://stackoverflow.com/questions/28771542/why-dont-clonenode-script-tags-execute
-          const script = document.createElement('script');
-          script.src = child.src;
-          script.text = child.text;
-          script.async = child.async;
-          contentRef.current.appendChild(script);
-        } else {
-          contentRef.current.appendChild(child.cloneNode(true));
+      const documentFragment = templateElement.content;
+      if (contentRef.current) {
+        contentRef.current.innerHTML = '';
+
+        for (let child of documentFragment.children) {
+          if (child.tagName === 'SCRIPT') {
+            if (!shouldInsertScript(child)) {
+              continue;
+            }
+            // https://stackoverflow.com/questions/28771542/why-dont-clonenode-script-tags-execute
+            const script = document.createElement('script');
+            script.src = child.src;
+            if (child.text) {
+              script.src = `data:application/javascript,${encodeURIComponent(child.text)}`;
+            };
+            script.async = child.async;
+            script.async = false;
+            script.defer = false;
+            // https://html.spec.whatwg.org/multipage/scripting.html#execute-the-script-block
+            // load event fires after execution for scripts from external files
+            // (i.e. any <script> with a src attribute)
+            const thenable = {
+              then(resolve) {
+                this.resolve = resolve;
+              }
+            };
+            script.addEventListener('load', () => {
+              thenable.resolve();
+              afterInsertScript();
+            });
+            scriptPromises.push(Promise.resolve(thenable));
+            contentRef.current.appendChild(script);
+          } else {
+            contentRef.current.appendChild(child.cloneNode(true));
+          }
         }
       }
+      Promise.all(scriptPromises).then(afterInsertHTML);
     }
-    afterInsertHTML();
-  }
 
-  useEffect(() => {
-    manipulateHTML(templateRef.current);
-  });
+    templateRef.current && manipulateHTML(templateRef.current);
+  }, [html, afterInsertHTML, afterInsertScript, shouldInsertScript]);
 
   return (
     <>
       <template dangerouslySetInnerHTML={{__html: html}} ref={templateRef}></template>
-      <div ref={contentRef}></div>
+      <div ref={contentRef} style={style}></div>
     </>
   );
   // const documentFragment =
